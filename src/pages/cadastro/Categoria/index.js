83db33d0c8cee091396efb1a49d463f4f47024eb
lucasbc92 <lbc92@hotmail.com>
2020-07-28 19:26:41 -0300

    Started Category register form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,21 @@ const CadastroCategoria = () => {
     return (
         <Layout>
             <h1>Cadastro de Categoria</h1>
+
+            <form>
+                <label>
+                Nome da Categoria:
+                <input
+                    type="text"
+                />
+                </label>
+
+                <button>
+                Cadastrar
+                </button>
+            </form>
+
+
             <Link to='/'>
                 Ir para Home
             </Link>
